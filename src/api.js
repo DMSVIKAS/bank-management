1231f28aeeba2d705fa553a0278807494f37fb02
DMS VIKAS <126855737+DMSVIKAS@users.noreply.github.com>
2026-09-14 22:56:20 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://bank-backend-sx1g.onrender.com"
+    : process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: API_URL,
 });
 
 api.interceptors.response.use(
