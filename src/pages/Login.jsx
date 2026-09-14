d695e89024344a66b98b68a0a56575ebae105385
DMS VIKAS <126855737+DMSVIKAS@users.noreply.github.com>
2026-09-15 00:10:19 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
-import heroImg from "../components/download.jpeg";
+import heroImg from "../components/log2.jpeg";
 
 export default function Login() {
   const [email, setEmail] = useState("");
